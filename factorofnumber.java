ca4bda9c520882358ae99a5c22fdb750b3ef2817
YADA ABHILASH <108734672+abhilashyada@users.noreply.github.com>
2024-06-19 15:32:47 +0530

    Add files via upload
    Write a program to print the factors of the given number.
    
    Note: A number F is considered as factor of the given number N, if N is exactly divisible by F (Remainder after dividing N with F is zero. N % F == 0)
    
    Input
    
    The input will be a single line containing a positive integer.
    
    Output
    
    The output should contain the factors of the given number each in a new line.
    
    Explanation
    
    For example, if the given number is 6, which is exactly divisible by 1, 2, 3, 86, then the output should be
    1
    2
    3
    6

---
+++ factorofnumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class factorofnumber {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        for (int i = 1;i<= N;i++ ) {
+            if (N%i == 0) {
+                System.out.println(i);
+            } 
+        } 
+    }
+}
